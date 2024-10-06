cca6af0b16b29cdb1755d29643043f74d058fa9c
Elvis S <elvissabanovic3@gmail.com>
2024-10-06 19:05:51 +0400

    feat: improve lib and add tests

--- threadsafe_zmq/src/lib.rs
+++ threadsafe_zmq/src/lib.rs
@@ -1,5 +1,4 @@
 mod error;
-mod utils;
 
 use crossbeam_channel::{
     select, unbounded, Receiver as CrossbeamReceiver, Sender as CrossbeamSender,
@@ -12,14 +11,12 @@ use zmq::{Context, PollEvents, Socket};
 static UNIQUE_INDEX: AtomicU16 = AtomicU16::new(0);
 const IN: usize = 0;
 const OUT: usize = 1;
-const DEFAULT_LINGER_TIME: i32 = 30;
 
 pub type ZmqByteStream = Vec<Vec<u8>>;
 pub type Sender = CrossbeamSender<ZmqByteStream>;
 pub type Receiver = CrossbeamReceiver<ZmqByteStream>;
 
 pub struct ChannelPair {
-    ctx: Context,
     z_sock: Socket,
     z_tx: Vec<Socket>,
     z_control: Vec<Socket>,
@@ -47,14 +44,13 @@ unsafe impl Send for ChannelPair {}
 unsafe impl Sync for ChannelPair {}
 
 impl ChannelPair {
-    pub fn new(ctx: Context, socket: Socket) -> Result<Arc<Self>, ChannelPairError> {
-        let z_tx = Self::new_pair(&ctx)?;
-        let z_control = Self::new_pair(&ctx)?;
+    pub fn new(socket: Socket) -> Result<Arc<Self>, ChannelPairError> {
+        let z_tx = Self::new_pair()?;
+        let z_control = Self::new_pair()?;
 
         let (tx_chan, rx_chan) = unbounded::<ZmqByteStream>();
 
         let mut channel_pair = Self {
-            ctx,
             z_tx,
             z_control,
             tx_chan,
@@ -249,7 +245,8 @@ impl ChannelPair {
         Ok(())
     }
 
-    fn new_pair(context: &Context) -> Result<Vec<Socket>, ChannelPairError> {
+    fn new_pair() -> Result<Vec<Socket>, ChannelPairError> {
+        let context = Context::new();
         let addr = format!("inproc://_channelpair_internal-{}", get_unique_id());
         let server_pair = context.socket(zmq::PAIR)?;
         server_pair.bind(&addr)?;
@@ -259,163 +256,101 @@ impl ChannelPair {
 
         Ok(vec![server_pair, client_pair])
     }
-
-    fn disconnect_socket(socket: &Socket) -> Result<(), ChannelPairError> {
-        match socket.get_last_endpoint()? {
-            Ok(endpoint) => socket
-                .disconnect(&endpoint)
-                .map_err(|e| ChannelPairError::Zmq(e)),
-
-            Err(bytes) => {
-                let endpoint = String::from_utf8_lossy(&bytes).into_owned();
-                socket
-                    .disconnect(&endpoint)
-                    .map_err(|e| ChannelPairError::Zmq(e))
-            }
-        }
-    }
 }
 
 fn get_unique_id() -> u16 {
     UNIQUE_INDEX.fetch_add(1, Ordering::SeqCst)
 }
 
-// #[test]
-//     fn channel_pair() {
-//         let (sb, ctx1, sc, ctx2) = new_pair_socket();
-//
-//         let num: usize = 10;
-//
-//         // Create a new WaitGroup and add 2 for each spawned thread
-//         let wait_group = WaitGroup::new();
-//         wait_group.add();
-//         wait_group.add();
-//
-//         // Create and run the echo thread
-//         {
-//             let wg = wait_group.clone();
-//             std::thread::spawn(move || {
-//                 let cp = ChannelPair::new(ctx1, sb).unwrap();
-//                 run_echo(num, &cp);
-//                 wg.done();
-//             });
-//         }
-//
-//         // Create and run the write thread
-//         {
-//             let wg = wait_group.clone();
-//             std::thread::spawn(move || {
-//                 let cc = ChannelPair::new(ctx2, sc).unwrap();
-//                 run_write(num, &cc);
-//                 wg.done();
-//             });
-//         }
-//
-//         let timeout = Duration::from_secs(5);
-//         if !wait_group.wait_timeout(timeout) {
-//             panic!("Test timed out after 10 seconds!");
-//         }
-//
-//         println!("All threads completed successfully.");
-//     }
-//
-//     #[test]
-//     fn channel_pair_test() {
-//         let ctx = Context::new();
-//
-//         // Create REP socket for the server
-//         let server_socket = ctx.socket(zmq::REP).expect("Failed to create REP socket");
-//         server_socket
-//             .bind("tcp://127.0.0.1:5555")
-//             .expect("Failed to bind server socket");
-//
-//         let client_ctx = Context::new();
-//         // Create REQ socket for the client
-//         let client_socket = client_ctx
-//             .socket(zmq::REQ)
-//             .expect("Failed to create REQ socket");
-//         client_socket
-//             .connect("tcp://127.0.0.1:5555")
-//             .expect("Failed to connect client socket");
-//
-//         // Create a WaitGroup to wait for the server thread to complete
-//         let wait_group = WaitGroup::new();
-//
-//         // Create a shared counter to track remaining messages
-//         let num_of_messages = Arc::new(Mutex::new(20)); // Initial message count set to 20
-//
-//         // Spawn the server thread using a ChannelPair
-//         wait_group.add();
-//         let wg = wait_group.clone();
-//         let num_of_messages_server = Arc::clone(&num_of_messages);
-//
-//         std::thread::spawn(move || {
-//             let channel_pair = ChannelPair::new(ctx, server_socket).unwrap();
-//
-//             while *num_of_messages_server.lock().unwrap() > 0 {
-//                 // Wait for a message from the client
-//                 match channel_pair.rx_chan().recv() {
-//                     Ok(message) => {
-//                         let message_clone = message.clone();
-//
-//                         // Spawn a new thread for each message to process it
-//                         let cp_clone = Arc::clone(&channel_pair);
-//                         let num_of_messages_server = Arc::clone(&num_of_messages_server);
-//                         std::thread::spawn(move || {
-//                             // Simulate message processing
-//                             std::thread::sleep(Duration::from_millis(5)); // Simulate processing time
-//
-//                             // Send the same message back as the response
-//                             cp_clone
-//                                 .tx_chan()
-//                                 .send(message_clone)
-//                                 .expect("Failed to send response");
-//
-//                             // Decrement the shared counter
-//                             let mut num = num_of_messages_server.lock().unwrap();
-//                             *num -= 1;
-//
-//                             // Exit the thread once done
-//                             if *num == 0 {
-//                                 println!("Server: All messages processed.");
-//                             }
-//                         });
-//                     }
-//                     Err(err) => {
-//                         eprintln!("Server: Failed to receive message: {:?}", err);
-//                         break;
-//                     }
-//                 }
-//             }
-//
-//             wg.done(); // Mark the server thread as done
-//         });
-//
-//         // Client side logic
-//         let mut num_of_messages_client = 20;
-//
-//         while num_of_messages_client > 0 {
-//             // Send a message to the server
-//             let msg = vec![b"Hello".to_vec()];
-//             client_socket
-//                 .send_multipart(&msg, 0)
-//                 .expect("Failed to send message from client");
-//
-//             // Wait for a response from the server
-//             let response = client_socket
-//                 .recv_multipart(0)
-//                 .expect("Failed to receive message from server");
-//             println!("Client: Received response: {:?}", response);
-//
-//             // Decrement the number of remaining messages
-//             num_of_messages_client -= 1;
-//         }
-//
-//         // Wait for the server to finish processing all messages, with a timeout of 10 seconds
-//         if !wait_group.wait_timeout(Duration::from_secs(5)) {
-//             panic!("Test timed out after 10 seconds!");
-//         }
-//
-//         println!("All messages processed successfully.");
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::{Arc, Mutex};
+    use std::time::Duration;
+
+    #[test]
+    fn channel_pair_test() {
+        let ctx = Context::new();
+
+        let server_socket = ctx
+            .socket(zmq::ROUTER)
+            .expect("Failed to create ROUTER socket");
+        server_socket
+            .bind("tcp://127.0.0.1:5555")
+            .expect("Failed to bind server socket");
+
+        let processed_messages = Arc::new(Mutex::new(20));
+        let channel_pair = ChannelPair::new(server_socket).expect("Failed to create ChannelPair");
+
+        // Spawn the server thread
+        let server_processed_messages = Arc::clone(&processed_messages);
+        std::thread::spawn(move || {
+            while *server_processed_messages.lock().unwrap() > 0 {
+                match channel_pair.rx_chan().recv() {
+                    Ok(message) => {
+                        let message_clone = message.clone();
+
+                        let cp_clone = Arc::clone(&channel_pair);
+                        let server_processed_messages = Arc::clone(&server_processed_messages);
+                        std::thread::spawn(move || {
+                            std::thread::sleep(Duration::from_millis(2)); // Simulate processing time
+
+                            // Send the same message back as the response
+                            cp_clone
+                                .tx_chan()
+                                .send(message_clone)
+                                .expect("Failed to send response");
+
+                            // Decrement the shared counter safely
+                            let mut counter = server_processed_messages.lock().unwrap();
+                            *counter -= 1;
+                        });
+                    }
+                    Err(err) => {
+                        panic!("Server: Timeout receive message: {:?}", err);
+                    }
+                }
+            }
+        });
+
+        // Client logic
+        let client_handle = std::thread::spawn(move || {
+            let client_ctx = Context::new();
+            let client_socket = client_ctx
+                .socket(zmq::DEALER)
+                .expect("Failed to create DEALER socket");
+            client_socket
+                .connect("tcp://127.0.0.1:5555")
+                .expect("Failed to connect client socket");
+            client_socket
+                .set_identity("client-1".as_bytes())
+                .expect("Failed to set client identity");
+
+            let mut num_of_messages_client = 20;
+            while num_of_messages_client >= 0 {
+                // Send a message to the server
+                let msg = vec![b"Hello".to_vec()];
+                client_socket
+                    .send_multipart(&msg, 0)
+                    .expect("Failed to send message from client");
+
+                // Wait for a response from the server
+                match client_socket.recv_multipart(0) {
+                    Ok(_) => {
+                        num_of_messages_client -= 1;
+                    }
+                    Err(e) => panic!("Client: Failed to receive response: {:?}", e),
+                }
+            }
+        });
+
+        client_handle.join().expect("Client thread failed");
+
+        // Check that all messages have been processed
+        assert_eq!(
+            *processed_messages.lock().unwrap(),
+            0,
+            "Not all messages were processed."
+        );
+    }
+}
