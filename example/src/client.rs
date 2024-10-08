968906aaa4003f714c697bcc8def71dd05d31144
Elvis S <elvissabanovic3@gmail.com>
2024-10-08 12:45:24 +0400

    feat: async

--- example/src/client.rs
+++ example/src/client.rs
@@ -1,67 +1,67 @@
-use env_logger;
+use crate::utils::to_string;
 use log::{error, info};
 use rand::Rng;
 use zmq::Context;
 
+mod utils;
+
 fn main() {
     env_logger::init();
 
-    let clients = 4;
-    let mut handles = Vec::with_capacity(clients);
+    let addr = "tcp://localhost:5555";
+    let ctx = Context::new();
+    let socket = ctx
+        .socket(zmq::DEALER)
+        .expect("Failed to create DEALER socket");
 
-    for i in 0..clients {
-        let client_id = i;
-        let handle = std::thread::spawn(move || {
-            let addr = "tcp://localhost:5555";
-            let ctx = Context::new();
-            let socket = ctx
-                .socket(zmq::DEALER)
-                .expect("Failed to create PAIR socket");
+    let id = format!("Client-{}", 1);
+    socket
+        .set_identity(id.clone().as_bytes())
+        .expect("Failed to set identity");
+    socket.connect(addr).expect("Failed to connect to server");
 
-            let rand_id = client_id as u8 + generate_random_number();
-            let id = format!("client-{}", rand_id);
-            socket
-                .set_identity(id.clone().as_bytes())
-                .expect("Failed to set identity");
-            socket.connect(addr).expect("Failed to connect to server");
-
-            info!("{} connected to: {}", id, addr);
-            loop {
-                let rand_num = generate_random_number();
-                let rand_num_bytes = rand_num.to_le_bytes().to_vec();
-
-                match socket.send_multipart(vec![rand_num_bytes], 0) {
-                    Ok(_) => info!("{}, sent number: {}", id, rand_num),
-                    Err(snd_err) => {
-                        error!("{}, failed to send message: {:?}", id, snd_err);
-                        continue;
-                    }
-                }
-
-                match socket.recv_multipart(0) {
-                    Ok(message) => {
-                        info!("Client {}, received result: {:?}", client_id, message);
-                    }
-                    Err(rcv_err) => {
-                        error!(
-                            "Client {}, failed to receive message: {:?}",
-                            client_id, rcv_err
-                        );
-                    }
-                }
-
-                std::thread::sleep(std::time::Duration::from_millis(100));
-            }
-        });
-        handles.push(handle);
-    }
-
+    info!("Connected to: {}", addr);
     loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        let rand_num = to_string(generate_random_number());
+
+        // Send the message to the server
+        match socket.send(rand_num.as_bytes(), 0) {
+            Ok(_) => {
+                info!("SND: fib({})=?", rand_num.clone());
+            }
+            Err(snd_err) => {
+                error!("SND: failed to send message: {:?}", snd_err);
+                continue;
+            }
+        }
+
+        // Receive the response from the server
+        match socket.recv_multipart(0) {
+            Ok(message) => {
+                for (_, frame) in message.iter().enumerate() {
+                    match String::from_utf8(frame.clone()) {
+                        Ok(str_frame) => {
+                            if str_frame.is_empty() {
+                                continue;
+                            }
+
+                            info!("RCV: fib({})={}", rand_num, str_frame);
+                            info!("--------------------------------------------------")
+                        }
+                        Err(e) => {
+                            error!("RCV: failed to convert frame to string: {:?}", e);
+                        }
+                    }
+                }
+            }
+            Err(rcv_err) => {
+                error!("RCV: failed to receive message: {:?}", rcv_err);
+            }
+        }
     }
 }
 
-fn generate_random_number() -> u8 {
+fn generate_random_number() -> u64 {
     let mut rng = rand::thread_rng();
-    rng.gen_range(0..=30)
+    rng.gen_range(1..=80)
 }
